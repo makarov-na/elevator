5a7d87e501e085be8710ed3b60f9be77859eac0c
Nikolay Makarov <makarov-na@yandex.ru>
2020-09-05 16:14:29 +0300

    Ограничение выходного сигнала по максимуму

--- assignment1.py
+++ assignment1.py
@@ -4,14 +4,14 @@ from sim.elevator import sim_run
 # Simulator Options
 options = {}
 options['FIG_SIZE'] = [8, 8]  # [Width, Height]
-options['PID_DEBUG'] = False
+options['PID_DEBUG'] = True
 
 # Physics Options
 options['GRAVITY'] = True
 options['FRICTION'] = True
 options['ELEVATOR_MASS'] = 1000
 options['COUNTERWEIGHT_MASS'] = 1000
-options['PEOPLE_MASS'] = 0
+options['PEOPLE_MASS'] = 100
 
 # Controller Options
 options['CONTROLLER'] = True
@@ -26,12 +26,14 @@ class Controller:
         self.prev_time = 0
         self.prevent_err = None
         self.output = 0
+        self.output_max = 5
 
         self.integr_err = 0
-       
-        self.kp = 0.2
-        self.kd = 0.75
+
+        self.kp = 2
+        self.kd = 2.5
         self.ki = 0
+        self.output_data = np.array([[0, 0, 0, 0]])
 
     def get_current_error(self):
         return self.current_err
@@ -43,15 +45,15 @@ class Controller:
 
         dt = t - self.prev_time
         self.prev_time = t
-        
+
         current_err = self.target_value - current_value
 
         p_out = self.kp*current_err
 
         #self.integr_err = self.integr_err + self.current_err
-        
+
         if self.prevent_err != None:
-           
+
             diff_err = current_err - self.prevent_err
             d_out = self.kd*diff_err/dt
         else:
@@ -64,6 +66,15 @@ class Controller:
         #    self.diff_err = self.current_err
         self.output = p_out + d_out + self.ki*self.integr_err
         self.prevent_err = current_err
+
+        if self.output > self.output_max:
+            self.output = self.output_max
+        if self.output_max < - self.output_max:
+            self.output_max = - self.output_max
+
+        i_out = 0
+        self.output_data = np.concatenate((self.output_data,np.array([[t, p_out, i_out, d_out]])))
+
 
         # INSERT CODE ABOVE.
         return self.output
