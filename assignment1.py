a67c2109ae5886f93b1aada9ce87bc954de57975
Nikolay Makarov <makarov-na@yandex.ru>
2020-09-12 22:46:59 +0300

    PID

--- assignment1.py
+++ assignment1.py
@@ -25,14 +25,16 @@ class Controller:
         self.target_value = reference
         self.prev_time = 0
         self.prevent_err = None
+        self.prevent_diff_err = None
         self.output = 0
         self.output_max = 5
 
         self.integr_err = 0
+        self.integration_windup = 5 
 
         self.kp = 2
-        self.kd = 2.5
-        self.ki = 0
+        self.kd = 2.4
+        self.ki = 0.2
         self.output_data = np.array([[0, 0, 0, 0]])
 
     def get_current_error(self):
@@ -50,31 +52,33 @@ class Controller:
 
         p_out = self.kp*current_err
 
-        #self.integr_err = self.integr_err + self.current_err
+        diff_err = 0
 
         if self.prevent_err != None:
-
             diff_err = current_err - self.prevent_err
             d_out = self.kd*diff_err/dt
+            
         else:
             d_out = 0
 
-        #self.diff_err = self.prevent_err - self.current_err
-        # if (abs(self.current_err) < 19):
-        #    self.integr_err = 0
-        # if (abs(self.current_err) == 19 and abs(self.prevent_err) == 19):
-        #    self.diff_err = self.current_err
-        self.output = p_out + d_out + self.ki*self.integr_err
+        print (dt)
+        self.integr_err = self.integr_err + current_err*dt
+        if self.integr_err * self.ki > self.output_max/self.integration_windup:
+            self.integr_err = (self.output_max/self.integration_windup)/self.ki    
+
+        i_out = self.ki*self.integr_err
+
+        self.output = p_out + d_out + i_out
         self.prevent_err = current_err
+        self.prevent_diff_err = diff_err
 
         if self.output > self.output_max:
             self.output = self.output_max
         if self.output_max < - self.output_max:
             self.output_max = - self.output_max
 
-        i_out = 0
+        
         self.output_data = np.concatenate((self.output_data,np.array([[t, p_out, i_out, d_out]])))
-
 
         # INSERT CODE ABOVE.
         return self.output
